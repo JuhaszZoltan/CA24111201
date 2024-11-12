c229a5422c0ff58400008b5bc9f133361c8e9265
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-11-12 10:29:10 +0100

    Add project files.

---
+++ Fish.cs
@@ -0,0 +1,44 @@
+﻿namespace CA24111201;
+
+internal class Fish
+{
+    private int top;
+    private float weight;
+    private bool weightIsSet = false;
+
+    public float Weight
+    {
+        get => weight; 
+        set
+        {
+            if (value < .5 || value > 40) throw new Exception(
+                $"value = {value}, a weight értéke csak [0.5; 40] intervallumon belül valid");
+
+            if (weightIsSet && (value < weight * .9 || value > weight * 1.1)) throw new Exception(
+                $"value = {value}, a weight értéke legfeljebb 10%-al változhat a jelenlegi értékéhez képest (+/-{weight*.1})");
+
+            weight = value;
+            weightIsSet = true;
+        }
+    }
+    public bool Predator { get; set; }
+    public int Top
+    {
+        get => top;
+        set
+        {
+            if (value < 0 || value > 400) throw new Exception(
+                $"a top értékét {value}-ra szeretnéd beállítani, ami a megngedett határon kívükl esik. A top [0; 400] intervallumon belül valid");
+
+            top = value;
+        }
+    }
+    public int Depth { get; set; }
+    public string Species { get; set; }
+
+    public override string ToString() => 
+        $"{Species} " +
+        $"({(Predator ? "carnivor" : "herbivor")}) " +
+        $"[{Top} - {Top + Depth} cm] " +
+        $"{Weight:00.0} kg";
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using CA24111201;
+
+Fish fish = new()
+{
+    Species = "Keszeg",
+    Predator = false,
+    Weight = 20.0F,
+    Top = 30,
+    Depth = 80,
+};
+
+fish.Weight += 2;
+
+Console.WriteLine(fish);
